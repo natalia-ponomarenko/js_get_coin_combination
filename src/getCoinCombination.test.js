caaac082c9ef0c736965ccf18871a15e7c005338
natalia-ponomarenko <virus.zabutiya@gmail.com>
2023-09-04 14:09:27 +0300

    solution

--- src/getCoinCombination.test.js
+++ src/getCoinCombination.test.js
@@ -4,7 +4,42 @@ describe('getCoinCombination', () => {
   const { getCoinCombination } = require('./getCoinCombination');
 
   it(`should be declared`, () => {
-    expect(getCoinCombination)
-      .toBeInstanceOf(Function);
+    expect(getCoinCombination).toBeInstanceOf(Function);
+  });
+
+  it(`should return an array`, () => {
+    const result = getCoinCombination(5);
+
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  it(`should return [0, 0, 0, 0] if cents === 0`, () => {
+    const result = getCoinCombination(0);
+
+    expect(result).toEqual([0, 0, 0, 0]);
+  });
+
+  it('should return [0, 0, 0, 1] if cents === 25', () => {
+    const result = getCoinCombination(25);
+
+    expect(result).toEqual([0, 0, 0, 1]);
+  });
+
+  it(`should return [1, 1, 0, 0] if cents === 6`, () => {
+    const result = getCoinCombination(6);
+
+    expect(result).toEqual([1, 1, 0, 0]);
+  });
+
+  it(`should return [2, 1, 1, 0] if cents === 17`, () => {
+    const result = getCoinCombination(17);
+
+    expect(result).toEqual([2, 1, 1, 0]);
+  });
+
+  it(`should return [0, 0, 0, 2] if cents === 50`, () => {
+    const result = getCoinCombination(50);
+
+    expect(result).toEqual([0, 0, 0, 2]);
   });
 });
